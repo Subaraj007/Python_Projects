fd431220fe489a6828b64dc4e2da30edc5dabbbf
B.Subaraj <subaraj1961@gmail.com>
2023-09-14 20:57:24 +0530

    Subset

---
+++ Find_subset.py
@@ -0,0 +1,9 @@
+#get how many test cases will you try
+test_case=int(input())
+list=[]
+for i in range(test_case):
+    num_ele_set_A=int(input())
+    set_A=input().split()
+    num_ele_set_B=int(input())
+    set_B=input().split()
+    
